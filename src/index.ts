1f81ac32aad5ccada5590666189329e8c8a38546
Andrew Israel <andrew@propelauth.com>
2022-07-04 13:52:39 -0700

    Export all the necessary types for other libraries

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,25 @@
-export {initBaseAuth} from "./auth"
+export {initBaseAuth, BaseAuthOptions, RequriedOrgInfo} from "./auth"
+export {TokenVerificationMetadata} from "./api"
+export {
+    CreateUserException,
+    ForbiddenException,
+    MagicLinkCreationException,
+    UnauthorizedException,
+    UnexpectedException,
+    UpdateUserEmailException,
+    UpdateUserMetadataException
+} from "./exceptions"
+export {
+    User,
+    Org,
+    OrgIdToOrgMemberInfo,
+    OrgMemberInfo,
+    toUser,
+    InternalOrgMemberInfo,
+    UserAndOrgMemberInfo,
+    InternalUser,
+    toOrgIdToOrgMemberInfo,
+    UserRole,
+    UserMetadata,
+    toUserRole
+} from "./user"
