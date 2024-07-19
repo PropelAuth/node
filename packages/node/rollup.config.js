1194568634d2c51410cf48d0abc6bb47a23d004f
Frank Blechschmidt <contact@frank-blechschmidt.com>
2024-07-18 22:16:29 -0700

    feat(npm): add worker support for node package

--- packages/node/rollup.config.js
+++ packages/node/rollup.config.js
@@ -11,8 +11,7 @@ const globals = {
     ...packageJson.devDependencies,
 }
 
-export default {
-    input: "src/index.ts",
+export default [{
     output: [
         {
             file: packageJson.main,
@@ -23,15 +22,31 @@ export default {
             file: packageJson.module,
             format: "esm",
             sourcemap: true,
-        },
+        }
     ],
+    resolveConfig: {
+        extensions,
+    },
+}, {
+    output: {
+        exports: "named",
+        format: "es",
+        file: "dist/browser/index.js",
+        sourcemap: true,
+    },
+    resolveConfig: {
+        extensions,
+        exportConditions: ["browser", "worker"],
+        browser: true,
+    },
+}].map(config => ({
+    input: "src/index.ts",
+    output: config.output,
     plugins: [
         peerDepsExternal(),
-        resolve({
-            extensions,
-        }),
+        resolve(config.resolveConfig),
         commonjs(),
         typescript(),
     ],
     external: Object.keys(globals),
-}
+}))
